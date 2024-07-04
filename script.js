8bb63c86c86e69e6f3281175f9a8d862f4869099
Kathleen <21-74785@g.batstate-u.edu.ph>
2024-07-04 16:38:54 +0800

    Added navigation bar

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-dfvvb
+document.addEventListener('DOMContentLoaded', (event) => {
+  const navbarLinks =document.querySelectorALL('.NAVBAR A');
+  navbarLinks.forEach(link => {
+    link.addEventListener ('click', (event) => {
+      event.preventDefault();
+      alert("You clicked on ${event.target.textContent}");
+    });
+
+  });
+  
+});
